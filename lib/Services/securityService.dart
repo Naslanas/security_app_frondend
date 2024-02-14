f9724465142149a56d703650122be046d6939ed2
Naslanas <naslava00@gmail.com>
2024-02-14 07:36:57 +0530

    add security updated

--- lib/Services/securityService.dart
+++ lib/Services/securityService.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:security_app/Models/securityModel.dart';
 class SecurityApiService{
@@ -10,6 +12,30 @@ class SecurityApiService{
     }
     else{
       return [];
+    }
+  }
+  
+  Future<dynamic>sendData(String name ,String Emp_id,String address,String phone,String email,String password)async{
+    var client=http.Client();
+    var apiUrl=Uri.parse("http://localhost:3001/api/securityapp/regsecurity");
+    var response=await client.post(apiUrl,
+    headers: <String,String>{
+      "Content-Type":"application/json;charset=UTF-8"
+    },
+    body: jsonEncode(<String,String>{
+      "name": name,
+      "empid": Emp_id,
+      "address": address,
+      "phone": phone,
+      "email": email,
+      "password": password,
+    })
+    );
+    if(response.statusCode==200){
+      return json.decode(response.body);
+    }
+    else{
+      throw Exception("failure to add");
     }
   }
 }
